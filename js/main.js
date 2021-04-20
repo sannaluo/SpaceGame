cf037821cfa941157d796eb9494d91e61b0e1609
sannaluo <sanna.luostarinen@metropolia.fi>
2021-04-20 15:40:37 +0300

    shoot balls to the center of the screen when press spacebar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import * as THREE from '/three.js-dev/build/three.module.js';
+import {OrbitControls} from '/three.js-dev/examples/jsm/controls/OrbitControls.js';
+import {shoot} from '/js/shooting.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,17 +9,63 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+// cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+cube.position.set(0, 0, -5);
+//camera.position.set(5, 10, 5);
+camera.position.set(0, 0, 2);
+//camera.lookAt(cube.position);
+
+
+// test cube behind camera
+const geometry3 = new THREE.BoxGeometry();
+const material3 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube3 = new THREE.Mesh( geometry3, material3 );
+scene.add( cube3 );
+cube3.position.set(0, 0, 5);
+
+//lights
+const directionalLight = new THREE.DirectionalLight( 0xFFECA4,0.2 );
+directionalLight.position.set( 0.1, 0.2, 1 );
+scene.add( directionalLight );
+const light = new THREE.AmbientLight( 0xFFF6D5, 0.4 ); // soft white light
+scene.add( light );
+
+
+// axes helper
+//const axesHelper = new THREE.AxesHelper( 5 );
+//scene.add( axesHelper );
+
+
+// orbit controls
+const controls = new OrbitControls( camera, renderer.domElement );
+
+// ship
+const geometry2 = new THREE.BoxGeometry();
+const material2 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const ship = new THREE.Mesh( geometry2, material2 );
+scene.add( ship );
+ship.position.set(0,0,0);
+ship.scale.set(0.5, 0.5, 0.5);
+ship.rotation.y = Math.PI / 4 ;
+
+function loadPlayerShip() {
+
+    ship.position.set(camera.position.x, camera.position.y - 1, camera.position.z - 1);
+
+}
 
 const animate = function () {
     requestAnimationFrame( animate );
 
     renderer.render( scene, camera );
+    loadPlayerShip();
+    shoot(camera, scene);
 };
 
 animate();
---
+++ js/shooting.js
@@ -0,0 +1,84 @@
+import * as THREE from '/three.js-dev/build/three.module.js';
+
+// Bullets array
+let bullets = [];
+let keyEvents = {};
+let player = {canShoot:0};
+
+// raycaster
+const raycaster = new THREE.Raycaster();
+const rayDirection = new THREE.Vector2();
+rayDirection.x = 0;
+rayDirection.y = 0;
+
+/**
+ * Shoots balls when spacebar is pressed down.
+ * @param camera
+ * @param scene
+ */
+export function shoot(camera, scene) {
+    raycaster.setFromCamera( rayDirection, camera );
+
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children );
+    for ( let i = 0; i < intersects.length; i ++ ) {
+        //   intersects[ i ].object.material.color.set( 0xff0000 );
+        //console.log(intersects[ i ].object.name);
+    }
+
+    // go through bullets array and update position
+    // remove bullets when appropriate
+    for(let index = 0; index < bullets.length; index += 1){
+        if( bullets[index] === undefined ) continue;
+        if( bullets[index].alive == false ){
+            bullets.splice(index,1);
+            continue;
+        }
+
+        bullets[index].position.add(bullets[index].velocity);
+    }
+
+    if(keyEvents[32] && player.canShoot <= 0) { // spacebar
+        // creates a bullet as a Mesh object
+        let bullet = new THREE.Mesh(
+            new THREE.SphereGeometry(1,8,8),
+            new THREE.MeshBasicMaterial({color:0xffffff})
+        );
+
+        bullet.scale.set(0.5, 0.5, 0.5);
+        bullet.position.set(camera.position.x, camera.position.y - 2, camera.position.z - 1);
+
+        console.log(rayDirection);
+        console.log(raycaster.ray.direction);
+
+        // set the velocity of the bullet
+        bullet.velocity = new THREE.Vector3(raycaster.ray.direction.x, raycaster.ray.direction.y, raycaster.ray.direction.z);
+
+        // after 1000ms, set alive to false and remove from scene
+        // setting alive to false flags our update code to remove
+        // the bullet from the bullets array
+        bullet.alive = true;
+        setTimeout(function(){
+            bullet.alive = false;
+            scene.remove(bullet);
+        }, 1000);
+
+        // add to scene, array, and set the delay to 10 frames
+        bullets.push(bullet);
+        scene.add(bullet);
+        player.canShoot = 10;
+    }
+    if(player.canShoot > 0) player.canShoot -= 1;
+}
+
+function keyDown(event){
+    event.preventDefault();
+    keyEvents[event.keyCode] = true;
+}
+
+function keyUp(event){
+    keyEvents[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
